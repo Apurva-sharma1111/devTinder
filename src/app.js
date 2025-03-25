508209fef0ad39272830a9c89d8486495bd40a1a
Apurva <avsharmaunique@gmail.com>
2025-03-25 13:21:34 +0530

    checked http request using postman

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.use("/test", (req, res) => {
-    res.send("Hello from test!!.")
+app.get("/user", (req, res) => {
+    res.send({firstName: "Apurva", lastName: "Sharma"})
 })
+
+app.post("/user", (req, res) => {
+    res.send("Data saved to DB")
+})
+
+app.delete("/user", (req, res) => {
+    res.send("Data deleted successfully.")
+})
+
 app.listen(3000, () => {
     console.log("Server running successfully")
 });
